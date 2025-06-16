3eb2f25e380945504f4a5567dba07795ce492190
ilkhoeri <37648374+ilkhoeri@users.noreply.github.com>
2025-06-16 10:53:09 +0700

    Update loading.tsx

--- app/(app)/chat/loading.tsx
+++ app/(app)/chat/loading.tsx
@@ -2,10 +2,10 @@ import { cookies } from 'next/headers';
 import { classResizable } from '@/resource/client/components/chat/chat-container';
 import { ContainerSkeleton } from '@/resource/client/components/chat/chat-skeleton';
 
-export default function Loading() {
+export default async function Loading() {
   const cookieStore = await cookies();
   const layout = cookieStore.get('__resizable-panels:layout:chat_container');
-  const defaultLayout = layout ? JSON.parse(layout.value) : undefined;
+  const defaultLayout = layout ? JSON.parse(layout.value) : [15, 30, 55];
 
   return <ContainerSkeleton layouts={defaultLayout}  className={classResizable.root} />;
 }
